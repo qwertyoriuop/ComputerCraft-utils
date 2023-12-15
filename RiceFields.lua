f0c960177e80a5d5d91fae5f99c6833c219e80e8
qwertyoriuop <41389168+qwertyoriuop@users.noreply.github.com>
2023-12-15 18:25:35 +0100

    New Rice

--- RiceFields.lua
+++ RiceFields.lua
@@ -1,103 +1,84 @@
 -- Function to check if the block below is rice
 function isRiceBelow()
     local success, data = turtle.inspectDown()
-    print("Checking for rice below: " .. (success and data.name or "none"))
-    return success and data.name == "farmersdelight:rice"
+    return success and (data.name == "farmersdelight:rice")
 end
 
 -- Function to check if the block below is a rice panicle
 function isRicePanicleBelow()
     local success, data = turtle.inspectDown()
-    print("Checking for rice panicle below: " .. (success and data.name or "none"))
-    return success and data.name == "farmersdelight:rice_panicles"
+    return success and (data.name == "farmersdelight:rice_panicles")
 end
 
 -- Function to check for mature rice panicle
 function checkMaturePanicle()
     local success, data = turtle.inspectDown()
-    if success then
-        if data.name == "farmersdelight:rice_panicles" then
-            print("Rice panicle found with age: " .. data.state.age)
-            if data.state.age == 3 then
-                print("Mature rice panicle found")
-                return true
-            else
-                print("Immature rice panicle found")
-                return false
-            end
-        else
-            print("Not a rice panicle below")
-            return false
-        end
-    else
-        print("Nothing to inspect below")
-        return false
-    end
+    return success and (data.name == "farmersdelight:rice_panicles" and data.metadata.age == 3)
 end
 
 -- Function to mine a mature crop
 function mineIfMature()
     if checkMaturePanicle() then
-        print("Mining mature panicle")
         turtle.digDown()
+        return true
     else
-        print("Mature panicle not found to mine")
+        return false
     end
 end
 
--- Function to check if the turtle is at the edge of the field
-function isAtFieldEdge()
-    local success, data = turtle.inspectDown()
-    print("Checking if at field edge")
-    -- Assuming that the presence of air or water signifies the edge of the field
-    return not success or data.name == "minecraft:air" or data.name == "minecraft:water"
-end
-
--- Function to explore for new rows
-function exploreForNewRows()
-    local moveCount = 0
-    local turnCount = 0
-    print("Exploring for new rows")
-    while turnCount < 4 do -- Try all four directions
-        if isAtFieldEdge() then
-            print("At field edge, turning right")
-            turtle.turnRight()
-            turnCount = turnCount + 1
+-- Function to move to next panicle
+function moveToNextPanicle()
+    local success = false
+    -- Check the front, right, and left for a panicle. If one exists, move to it.
+    local directions = {turtle.forward, turtle.turnRight, turtle.forward, turtle.turnLeft, turtle.forward}
+    for i, action in ipairs(directions) do
+        if i == 2 or i == 4 then
+            action() -- turn, don't check
         else
-            print("Moving forward to find new row")
-            turtle.forward()
-            moveCount = moveCount + 1
-            if isRiceBelow() or isRicePanicleBelow() then
-                -- Found a new row to harvest
-                print("New row found to harvest")
-                return true
+            action()
+            if isRicePanicleBelow() then
+                success = true
+                break
             end
         end
     end
-
-    -- If a new row wasn't found, back up to the original position and return false
-    print("New row not found, backtracking")
-    for i = 1, moveCount do
-        turtle.back()
-    end
-    return false
+    return success
 end
 
--- Function to follow the rice pattern
-function ricePattern()
-    print("Starting rice pattern")
-    while true do
-        mineIfMature()
-        if isAtFieldEdge() then
-            -- At the edge, so explore for new rows
-            if not exploreForNewRows() then
-                print("No new rows found, ending pattern.")
-                break -- No more rows to harvest, end the pattern
+-- Function to start the harvesting process
+function startHarvesting()
+    -- If turtle starts on top of a panicle, mine it.
+    if not mineIfMature() then
+        -- If it wasn't a mature panicle, or there was no panicle, move down to check for rice.
+        turtle.down()
+        if not isRiceBelow() then
+            -- If there's no rice below, we're in the air and need to move to find the rice layer.
+            if not moveToNextPanicle() then
+                -- If we can't find the next panicle, the field might be harvested, and we can stop.
+                print("No more panicles found. Stopping.")
+                return
             end
-        else
-            -- Not at the edge, so keep moving forward
-            print("Not at field edge, moving forward")
+        end
+        -- Now we should be on the rice layer, move back up to the panicle layer.
+        turtle.up()
+    end
+    -- Continue with the next panicle.
+    moveToNextPanicle()
+end
+
+-- Main function to handle the pattern
+function ricePattern()
+    while true do
+        startHarvesting()
+        if isRiceBelow() then
+            -- We expect to be above a rice block now, so let's move forward to the next panicle.
             turtle.forward()
+        else
+            -- If we find ourselves not above a rice block, we should search for the next panicle.
+            if not moveToNextPanicle() then
+                print("No more panicles found. Stopping.")
+                break
+            end
         end
     end
 end
