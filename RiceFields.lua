ed830c6b5685f95b50205c1d6e78a60ffa0666f4
qwertyoriuop <41389168+qwertyoriuop@users.noreply.github.com>
2023-12-15 17:43:30 +0100

    revised rice

--- RiceFields.lua
+++ RiceFields.lua
@@ -4,86 +4,52 @@ function isRiceBelow()
     return success and data.name == "farmersdelight:rice"
 end
 
-function isRiceFanicleBelow()
+-- Function to check if the block below is a rice panicle
+function isRicePanicleBelow()
     local success, data = turtle.inspectDown()
     return success and data.name == "farmersdelight:rice_panicles"
 end
 
-function goTillMature()
-    print("going till mature is found")
-    turtle.up()
-    turtle.forward()
-    if isRiceFanicleBelow() then
-        if checkMatureFanicle() then
-            turtle.digDown()
-            turtle.down()
-        end
+-- Function to check for mature rice panicle
+function checkMaturePanicle()
+    local success, data = turtle.inspectDown()
+    if success and data.name == "farmersdelight:rice_panicles" and data.state.age == 3 then
+        print("Mature rice panicle found")
+        return true
     end
+    return false
 end
 
-function checkMatureFanicle()
-    local success, data = turtle.inspectDown()
-    if success then
-        if data.name == "farmersdelight:rice_panicles" then
-            print("rice fanicle found")
-            if data.state.age == 3 then
-                print("mature rice fanicle found")
-                return true
-            else
-                print("immature rice fanicle found")
-                return false
-            end
-        else
-            print("not rice fanicle")
-            return false
+-- Function to move to the next rice or panicle block
+function moveToNextRiceOrPanicle()
+    local moved = false
+    for i = 1, 4 do  -- Check all four directions
+        if isRiceBelow() or isRicePanicleBelow() then
+            return true  -- Found rice or panicle in the current position
         end
-    else
-        print("no block below")
-        return false
+        turtle.turnRight()
+        turtle.forward()
+        moved = true
     end
-end
--- Function to check if the block in front is a mature rice crop
-function isMature()
-    local success, data = turtle.inspect()
-    if success then
-        -- Check for mature rice crop
-        -- return data.name == "farmersdelight:rice" and data.state.age == 3
-        if isRiceBelow() then
-            print(data.state.age)
-            if data.state.age == 3 then
-                return data.state.age == 3
-            else
-                goTillMature()
-                print("Not mature")
-            end
-        else
-            print("Not rice")
-            return false
-        end
-    else
+
+    if not moved then
+        print("No rice or panicle nearby")
         return false
     end
 end
 
 -- Function to mine a mature crop
 function mineIfMature()
-    if isMature() then
-        turtle.dig()
+    if checkMaturePanicle() then
+        turtle.digDown()
     end
 end
 
--- Function to move in the pattern you described
+-- Function to follow the rice pattern
 function ricePattern()
-    while isRiceBelow() do
+    while isRicePanicleBelow() or moveToNextRiceOrPanicle() do
         mineIfMature()
         turtle.forward()
-    end
-
-    -- Move one block to the side
-    turtle.turnRight()
-    if isRiceBelow() then  -- Check if there is rice in the next row
-        turtle.forward()
-        turtle.turnLeft()
     end
 end
 
